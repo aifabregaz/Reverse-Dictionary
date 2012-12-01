c0e5fbb3c496a4079797970df758231aa562fc5e
Artem Stepanyuk <faulknercs@yandex.ru>
2012-12-01 02:20:30 +0200

    Added reverse comparer

---
+++ ReverseDictionary/ReverseStringComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace ReverseDictionary
+{
+    class ReverseStringComparer : IComparer<String>
+    {
+        public int Compare(string x, string y)
+        {
+            var comparer = new CaseInsensitiveComparer();
+            return comparer.Compare(new String(x.Reverse().ToArray()), new String(y.Reverse().ToArray()));
+        }
+    }
+}
