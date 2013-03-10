6708798b57c8d494a3af453fdc4b48e241783a6a
Artem Stepanyuk <faulknercs@yandex.ru>
2013-03-11 01:38:03 +0200

    Added loading text from doc files (Issue #8)

--- TextLoader/FileLoader/DocTextLoader.cs
+++ TextLoader/FileLoader/DocTextLoader.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using Word = NetOffice.WordApi;
 
 namespace TextLoader.FileLoader
 {
     internal class DocTextLoader : ITextLoader
     {
+        private String _text;
+
         public void LoadFile(String path)
         {
-            throw new NotImplementedException();
+            using(var wordApplication = new Word.Application())
+            {
+                Word.Document newDocument = wordApplication.Documents.Open(path);
+
+                _text = newDocument.Content.Text;
+
+                wordApplication.Quit();
+            }
         }
 
         public String ExtractText()
         {
-            throw new NotImplementedException();
+            return _text;
         }
 
         public override string ToString()
